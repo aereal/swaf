d99856c327ce1b99d21dd7d2bce5dbe7d50567d9
aereal <trasty.loose@gmail.com>
2011-06-03 20:31:28 +0900

    implemented Swaf#headeer

--- lib/swaf.rb
+++ lib/swaf.rb
@@ -1,7 +1,36 @@
 # encoding: utf-8
 
+require "forwardable"
+require "swf_ruby"
+
 require "swaf/version"
 
 class Swaf
+	extend Forwardable
+
+	def self.load(doc)
+		(swaf = self.new).load!(doc)
+		swaf
+	end
+
+	def self.load_file(filename)
+		(swaf = self.new).load!(
+			File.binread(filename)
+		)
+		swaf
+	end
+
+	attr_reader :dumper
+	def_delegators :@dumper, *[
+		:header, :swf, :tags, :tags_addresses, :character_ids
+	]
+
+	def initialize
+		@dumper = SwfRuby::SwfDumper.new
+	end
+
+	def load!(swf)
+		self.dumper.dump(swf)
+	end
 end
 
--- spec/swaf_spec.rb
+++ spec/swaf_spec.rb
@@ -1,7 +1,23 @@
 # encoding: utf-8
 
+require "pathname"
 require "swaf"
 
 describe Swaf do
+	let(:prefix)     { Pathname(__FILE__).expand_path.parent }
+	let(:swf_file)   { prefix + 'samples' + 'mock.swf' }
+	let(:swf_dumper) { SwfRuby::SwfDumper.new }
+
+	before do
+		swf_dumper.open(swf_file)
+	end
+
+	subject do
+		Swaf.load_file(swf_file)
+	end
+
+	it "has header of SWF" do
+		subject.header.should == swf_dumper.header
+	end
 end
 
