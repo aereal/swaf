36ba167e13b5dbcf70f3bf7815d5d1b6fa37ae8c
aereal <trasty.loose@gmail.com>
2011-06-06 20:50:46 +0900

    put test in order

--- spec/swaf_spec.rb
+++ spec/swaf_spec.rb
@@ -11,11 +11,6 @@ describe Swaf do
 		Swaf.load_file(swf_file)
 	end
 
-	it "replace value of ActionScript's variable with specified value" do
-		subject.replace(:_itemname => 'fugafuga').swf.should ==
-			(prefix + 'samples/replaced_as_var.swf').binread
-	end
-
 	context "when detecting tags" do
 		let(:jpg_offset) { 1370 }
 
@@ -26,18 +21,21 @@ describe Swaf do
 
 	context "when replacing objects" do
 		let(:jpeg_replaced_swf) {
-			(prefix + 'samples' + 'jpeg_replaced.swf').open('rb:ASCII-8BIT') {|f|
-				f.read
-			}
+			(prefix + 'samples' + 'jpeg_replaced.swf').binread
 		}
 		let(:new_image) {
-			(prefix + 'samples' + 'another_fig.jpg').open('rb:ASCII-8BIT') {|f|
-				f.read
-			}
+			(prefix + 'samples' + 'another_fig.jpg').binread
+		}
+		let(:as_var_replaced_swf) {
+			(prefix + 'samples' + 'replaced_as_var.swf').binread
 		}
 
 		it "replace JPEG images with specified JPEG image" do
 			subject.replace_jpeg(1 => new_image).swf.should == jpeg_replaced_swf
+		end
+
+		it "replace value of ActionScript's variable with specified value" do
+			subject.replace(:_itemname => 'fugafuga').swf.should == as_var_replaced_swf
 		end
 	end
 end
