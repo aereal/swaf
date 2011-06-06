cced2626f0df1a25a4781ad9c46accfe9fd9795a
aereal <trasty.loose@gmail.com>
2011-06-06 20:48:21 +0900

    remove test about nonsense feature

--- spec/swaf_spec.rb
+++ spec/swaf_spec.rb
@@ -6,18 +6,9 @@ require "swaf"
 describe Swaf do
 	let(:prefix)     { Pathname(__FILE__).expand_path.parent }
 	let(:swf_file)   { prefix + 'samples' + 'mock.swf' }
-	let(:swf_dumper) { SwfRuby::SwfDumper.new }
-
-	before do
-		swf_dumper.open(swf_file)
-	end
 
 	subject do
 		Swaf.load_file(swf_file)
-	end
-
-	it "has header of SWF" do
-		subject.header.should == swf_dumper.header
 	end
 
 	it "replace value of ActionScript's variable with specified value" do
